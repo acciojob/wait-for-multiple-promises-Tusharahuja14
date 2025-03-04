949e8d085dc3622100ce5a2089801b20d322ecd2
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-03-05 01:53:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,59 +1,45 @@
-//your JS code here. If required.
 function getrandom() {
-	return Math.floor(Math.random()*2000)+1000;
+  return Math.floor(Math.random() * 2000) + 1000;
 }
-let ttime=0;
-let table=document.getElementById("output");
+
+let table = document.getElementById("output");
 table.innerHTML = `<tr id="loading"><td colspan="2">Loading...</td></tr>`;
-let promise1=()=>{
-	let time=getrandom();
-let	stime=time;
-	ttime+=time;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			let row=`<tr><td>Promise 1</td><td>${(stime/1000).toFixed(3)}</td></tr>`
-			resolve(row);
-		},time);
-	});
+
+let promise1 = () => {
+  let time = getrandom();
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let row = `<tr><td>Promise 1</td><td>${(time / 1000).toFixed(3)}</td></tr>`;
+      resolve({ row, time });
+    }, time);
+  });
 };
-	//your JS code here. If required.
-let promise2=()=>{
-	let time=getrandom();
-let	stime=time;
-	ttime+=time;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			let row=`<tr><td>Promise 2</td><td>${(stime/1000).toFixed(3)}</td></tr>`
-			resolve(row);
-		},time);
-	});
-}
-//your JS code here. If required.
-let promise3=()=>{
-	
-	let time=getrandom();
-let	stime=time;
-	ttime+=time;
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			let row=`<tr><td>Promise 3</td><td>${(stime/1000).toFixed(3)}</td></tr>`
-			resolve(row);
-		},time)
-	});
-}
-	//your JS code here. If required.
-let total=()=>{
-	let time=ttime
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			let row=`<tr><td style="font-weight:bold">Total</td><td>${(time/1000).toFixed(3)}</td></tr>`
-			resolve(row);
-		},ttime)
-	});
-}
-	Promise.all([promise1(),promise2(),promise3(),total()]).then
-((data)=>{let table=document.getElementById("output");
- table.innerHTML=data;
-	 }
-	)
-	
+
+let promise2 = () => {
+  let time = getrandom();
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let row = `<tr><td>Promise 2</td><td>${(time / 1000).toFixed(3)}</td></tr>`;
+      resolve({ row, time });
+    }, time);
+  });
+};
+
+let promise3 = () => {
+  let time = getrandom();
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let row = `<tr><td>Promise 3</td><td>${(time / 1000).toFixed(3)}</td></tr>`;
+      resolve({ row, time });
+    }, time);
+  });
+};
+
+Promise.all([promise1(), promise2(), promise3()]).then((data) => {
+  let totalTime = data.reduce((sum, item) => sum + item.time, 0); // Sum all times
+
+  let totalRow = `<tr><td style="font-weight:bold">Total</td><td>${(totalTime / 1000).toFixed(3)}</td></tr>`;
+
+  let table = document.getElementById("output");
+  table.innerHTML = data.map((item) => item.row).join("") + totalRow;
+});
